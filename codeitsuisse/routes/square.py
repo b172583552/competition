974f55a0992d4dbc0adc27e389bcecd88ebb53f9
Wong Man Lok <u3593122@connect.hku.hk>
2022-09-17 17:08:37 +0800

    Update square.py

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -4,7 +4,7 @@ import math
 from flask import request, jsonify
 
 from codeitsuisse import app
-
+import datetime
 logger = logging.getLogger(__name__)
 
 @app.route('/square', methods=['POST'])
@@ -216,9 +216,95 @@ def cryptocollapz():
     logging.info("My result :{}".format(data))
     return json.dumps(data)
 
-@app.route('/calendarDays', methods=['POST'])
-def calendarDays():
+
+
+@app.route('/calendarday', methods=['POST'])
+def calendarday():
     data = request.get_json()
-    numbers = data.get("numbers")
-    numbers[1:] = (i for i in numbers if i > 0 and i < 366)
-    return json.dumps(numbers)
+    logging.info("data sent for evaluation {}".format(data))
+    data_ls = data.get("numbers")
+
+
+    y=data_ls[0]
+    result=[]
+    #convert number to string like "2022-01-01-5" and make them in a list
+    for i in range(1, len(data_ls)):
+        day = data_ls[i]
+        first_day = datetime.date(y, 1, 1)
+        wanted_day = first_day + datetime.timedelta(day - 1)
+        week = datetime.datetime.weekday(wanted_day)
+        wanted_day = str(wanted_day) + '-' + str(week)
+        result.append(wanted_day)
+
+    # judge
+    if result == []:
+        out = "       ,       ,       ,       ,       ,       ,       ,       ,       ,       ,       ,       ,"
+        resultJson = {}
+        logging.info("My result :{}".format(out))
+        resultJson["part1"] = out
+        return json.dumps(resultJson)
+
+    week_info = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [], 12: []}
+    for d in result:
+        year=d[0:4]
+        if int(year)!=y:
+            continue
+        week = int(d[11:])
+        month = d[5:7]
+        if month[0] == '0':
+            month = int(month[-1])
+        elif month == '11':
+            month = 11
+        elif month == '12':
+            month = 12
+        week_info[month].append(week)
+
+
+
+
+    out=""
+    for key in week_info:
+        if (week_info[key]==[]):
+            out=out+"       ,"
+        elif (1 in week_info[key]) and (2 in week_info[key]) and (3 in week_info[key]) and (4 in week_info[key]) and (5 in week_info[key]) and (6 in week_info[key]) and (7 in week_info[key]) :
+            out=out+"alldays,"
+        elif (1 not in week_info[key]) and (2 not in week_info[key]) and (3 not in week_info[key]) and (4 not in week_info[key]) and (5 not in week_info[key]) and (6 in week_info[key]) and (7 in week_info[key]) :
+            out = out + "weekend,"
+        elif (1 in week_info[key]) and (2 in week_info[key]) and (3 in week_info[key]) and (4 in week_info[key]) and (5 in week_info[key]) and (6 not in week_info[key]) and (7 not in week_info[key]) :
+            out=out+"weekday,"
+        else:
+            temp=""
+            if (1 in week_info[key]):
+                temp+="m"
+            else:
+                temp+=" "
+            if (2 in week_info[key]):
+                    temp += "t"
+            else:
+                    temp += " "
+            if (3 in week_info[key]):
+                temp+="w"
+            else:
+                temp+=" "
+            if (4 in week_info[key]):
+                temp+="t"
+            else:
+                temp+=" "
+            if (5 in week_info[key]):
+                temp+="f"
+            else:
+                temp+=" "
+            if (6 in week_info[key]):
+                temp+="s"
+            else:
+                temp+=" "
+            if (7 in week_info[key]):
+                temp+="s"
+            else:
+                temp+=" "
+            out=out+temp+','
+
+    resultJson={}
+    logging.info("My result :{}".format(out))
+    resultJson["part1"] = out
+    return json.dumps(resultJson)
