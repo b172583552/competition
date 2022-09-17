23d09f59f99c7358f13951b68acb525997cb8038
Wong Man Lok <u3593122@connect.hku.hk>
2022-09-17 09:40:50 +0800

    g

---
+++ test/test.py
@@ -0,0 +1,10 @@
+import requests
+website = https://creditsuissecompetition.herokuapp.com/
+payload = {'key1': 'value1', 'key2': 'value2'}
+#r = requests.get(website)
+#r = requests.post(website, data=payload)
+'''
+r = requests.delete('https://httpbin.org/delete')
+r = requests.head('https://httpbin.org/get')
+r = requests.options('https://httpbin.org/get')
+'''
